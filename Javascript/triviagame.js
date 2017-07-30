d0e3e44f98ee0b50b839c61b5745e3cb1c3748f4
Francisco Itamar Matos <itamarmatos@yahoo.com>
2017-07-30 12:49:07 -0500

    game logic

--- Javascript/triviagame.js
+++ Javascript/triviagame.js
@@ -1,1 +1,79 @@
-triviagame.js
+var firstQuestion = 
+{
+	'questionNumber': 1,
+	'timeRemaining': 30,
+	'questionScript': "What is Bubba’s real, full name?",
+	'potentialAnswers':
+	[
+	'Benjamin Buford Blue',
+	'Benjamin Buffa Blue',
+	'Benjamin Bubba Blue',
+	'Benjamin Beaufort Blue'
+	],
+	"correctAnswer":'Benjamin Buford Blue',
+	
+	'imageGif':"https://media.giphy.com/media/u29Biz9kyqkFO/giphy.gif",
+}
+var secondQuestion = 
+{
+	'questionNumber': 2,
+	'timeRemaining': 30,
+	'questionScript': "What number bus was Forrest supposed to catch to reach Jenny’s house?",
+	'potentialAnswers':
+	[
+	'6',
+	'9',
+	'19',
+	'26'
+	],
+	"correctAnswer":'9',
+	
+	'imageGif':"https://media.giphy.com/media/PHFmJ1YBSjDMs/giphy.gif",
+}
+var thirdQuestion = 
+	{
+	'questionNumber': '3',
+	'timeRemaining': 30,
+	'questionScript': "What is Forrest’s IQ?",
+	'potentialAnswers':
+	[
+		'65',
+		'75',
+		'85',
+		'155'
+	],
+	"correctAnswer":'75',
+	'imageGif':'https://media.giphy.com/media/EHbWV5FXHKxMY/giphy.gif',
+}
+var forthQuestion= 
+	{
+	'questionNumber': '4',
+	'timeRemaining': 30,
+	'questionScript': "What is the name of the security guard that Forrest speaks to when he reposrts the Watergate burglary?",
+	'potentialAnswers':
+	[
+		'Frank Wills',
+		'Luther Willis',
+		'Steve Evans',
+		'Walter Gate'
+	],
+	"correctAnswer":'Walter Gate',
+	'imageGif':"https://media.giphy.com/media/wRxT9xrYoxw9q/giphy.gif",
+
+	}
+var fifthQuestion = 
+{
+	'questionNumber': '5',
+	'timeRemaining': 30,
+	'questionScript': "What foods did Forrest use to describe himself and Jenny?",
+	'potentialAnswers':
+	[
+		'Fish & Chips',
+		'Peas & Carrots',
+		'Meat & Potatoes',
+		'Lemosn & Limes'
+	],
+	"correctAnswer":'Peas & Carrots',
+		'imageGif':"https://media.giphy.com/media/kYigEXoyjkkbS/giphy.gif",
+
+	}
